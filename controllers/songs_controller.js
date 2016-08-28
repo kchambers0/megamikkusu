8760b3c9330496f848ef6889cf4af9ae8fa81a35
Kevin Chambers <kevinchambers@Kevins-MacBook-Air.local>
2016-08-27 23:07:14 -0700

    basic controller setup

--- controllers/songs_controller.js
+++ controllers/songs_controller.js
@@ -1,5 +1,22 @@
 var mongoose = require('mongoose'),
+	url = require('url'),
 	Songs = mongoose.model('Songs');
+exports.getSongIds = function(req, res){
+	Songs.find().select({_id:1}).exec(function(err, songIds){
+		if(err || songIds.length < 1){
+			res.json(404, {msg:"Songs not Found."});
+		} else {
+			res.json(songIds);
+		}
+	});
+}
 exports.getSong = function(req, res){
-	
+	var query = url.parse(req.url, true).query;
+	Songs.findOne({_id:query.id}).exec(function(err, song){
+		if(!song){
+			res.json(404, {msg:"Song not Found."});
+		} else {
+			res.json(song);
+		}
+	});
 }
--- radio_routes.js
+++ radio_routes.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 module.exports = function(server){
 	var songs = require('./controllers/songs_controller');
-	app.use('/static', express.static('./static')).
+	server.use('/static', express.static('./static')).
 		use('/images', express.static('./static/images')).
 		use('/music' express.static('../../../Music/iTunes/iTunes\ Media/Music'));
-	app.get('/', function(req, res){
+	server.get('/', function(req, res){
 		res.render('radio');
 	});
-	app.get('/song/get', songs.getSong());
+	server.get('/song/getIds', songs.getSongIds());
+	server.get('/song/get', songs.getSong());
 }
